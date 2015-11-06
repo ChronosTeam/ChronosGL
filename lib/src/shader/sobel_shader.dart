d19840ec93dde95ce393dc8831afd9e199f46ee8
Robert Muth <robert@muth.org>
2015-11-05 23:14:32 -0500

    Bug fixes in sobel shader

--- lib/src/shader/sobel_shader.dart
+++ lib/src/shader/sobel_shader.dart
@@ -15,21 +15,21 @@ List<ShaderObject> createSobelShader() {
       ..AddUniformVar(uTextureSampler, "colorSampler")
       ..AddUniformVar(uCanvasSize, "size")
       ..SetBody([
-        """"
+        """
   float lum(vec4 c) {
     return dot(c.xyz, vec3(0.3, 0.59, 0.11));
   }
 
   float sobel() {
       vec2 imageIncrement = vec2(1.0/size.x,1.0/size.y);
-      float t00 = lum(texture2D(colorSampler, vTextureCoord + imageIncrement * vec2(-1, -1)));
-      float t10 = lum(texture2D(colorSampler, vTextureCoord + imageIncrement * vec2( 0, -1)));
-      float t20 = lum(texture2D(colorSampler, vTextureCoord + imageIncrement * vec2( 1, -1)));
-      float t01 = lum(texture2D(colorSampler, vTextureCoord + imageIncrement * vec2(-1,  0)));
-      float t21 = lum(texture2D(colorSampler, vTextureCoord + imageIncrement * vec2( 1,  0)));
-      float t02 = lum(texture2D(colorSampler, vTextureCoord + imageIncrement * vec2(-1,  1)));
-      float t12 = lum(texture2D(colorSampler, vTextureCoord + imageIncrement * vec2( 0,  1)));
-      float t22 = lum(texture2D(colorSampler, vTextureCoord + imageIncrement * vec2( 1,  1)));
+      float t00 = lum(texture2D(colorSampler, vTextureCoordinates + imageIncrement * vec2(-1, -1)));
+      float t10 = lum(texture2D(colorSampler, vTextureCoordinates + imageIncrement * vec2( 0, -1)));
+      float t20 = lum(texture2D(colorSampler, vTextureCoordinates + imageIncrement * vec2( 1, -1)));
+      float t01 = lum(texture2D(colorSampler, vTextureCoordinates + imageIncrement * vec2(-1,  0)));
+      float t21 = lum(texture2D(colorSampler, vTextureCoordinates + imageIncrement * vec2( 1,  0)));
+      float t02 = lum(texture2D(colorSampler, vTextureCoordinates + imageIncrement * vec2(-1,  1)));
+      float t12 = lum(texture2D(colorSampler, vTextureCoordinates + imageIncrement * vec2( 0,  1)));
+      float t22 = lum(texture2D(colorSampler, vTextureCoordinates + imageIncrement * vec2( 1,  1)));
       vec2 grad;
       grad.x = t00 + 2.0 * t01 + t02 - t20 - 2.0 * t21 - t22;
       grad.y = t00 + 2.0 * t10 + t20 - t02 - 2.0 * t12 - t22;
