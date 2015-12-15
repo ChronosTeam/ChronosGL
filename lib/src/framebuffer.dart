8b8686f6cd61c58ad983ac59986a9571cd8f93ae
Robert Muth <robert@muth.org>
2015-12-15 08:26:22 -0500

    stricter types and new constructure

--- lib/src/framebuffer.dart
+++ lib/src/framebuffer.dart
@@ -2,16 +2,14 @@ part of chronosgl;
 
 class ChronosFramebuffer {
   WEBGL.RenderingContext gl;
-  int width;
-  int height;
 
   WEBGL.Framebuffer framebuffer;
   WEBGL.Renderbuffer renderbuffer;
-  Texture colorTexture;
-  Texture depthTexture;
+  TypedTexture colorTexture;
+  TypedTexture depthTexture;
   var depthTextureExt;
 
-  ChronosFramebuffer(this.gl, this.width, this.height) {
+  ChronosFramebuffer(this.gl, width, height) {
     depthTextureExt = GetGlExtensionDepth(gl);
     // http://blog.tojicode.com/2012/07/using-webgldepthtexture.html
     if (depthTextureExt == null) {
@@ -20,27 +18,45 @@ class ChronosFramebuffer {
 
     framebuffer = gl.createFramebuffer();
 
-    colorTexture =
-        new TypedTexture("frame::color", width, height, 
-            WEBGL.RGB, WEBGL.UNSIGNED_BYTE);
+    colorTexture = new TypedTexture(
+        "frame::color", width, height, WEBGL.RGB, WEBGL.UNSIGNED_BYTE);
     colorTexture.Install(gl);
-    depthTexture = new TypedTexture("frame::depth", width, height, 
+    depthTexture = new TypedTexture("frame::depth", width, height,
         WEBGL.DEPTH_COMPONENT, WEBGL.UNSIGNED_SHORT);
     depthTexture.Install(gl);
 
     gl.bindFramebuffer(WEBGL.FRAMEBUFFER, framebuffer);
-    gl.framebufferTexture2D(WEBGL.FRAMEBUFFER, WEBGL.COLOR_ATTACHMENT0, WEBGL.TEXTURE_2D,
-        colorTexture.GetTexture(), 0);
-    gl.framebufferTexture2D(
-        WEBGL.FRAMEBUFFER, WEBGL.DEPTH_ATTACHMENT, WEBGL.TEXTURE_2D, depthTexture.GetTexture(), 0);
+    gl.framebufferTexture2D(WEBGL.FRAMEBUFFER, WEBGL.COLOR_ATTACHMENT0,
+        WEBGL.TEXTURE_2D, colorTexture.GetTexture(), 0);
+    gl.framebufferTexture2D(WEBGL.FRAMEBUFFER, WEBGL.DEPTH_ATTACHMENT,
+        WEBGL.TEXTURE_2D, depthTexture.GetTexture(), 0);
 
     gl.bindTexture(WEBGL.TEXTURE_2D, null);
     gl.bindFramebuffer(WEBGL.FRAMEBUFFER, null);
   }
 
+  ChronosFramebuffer.FromTexture(this.gl, this.colorTexture) {
+    framebuffer = gl.createFramebuffer();
+
+    gl.bindFramebuffer(WEBGL.FRAMEBUFFER, framebuffer);
+    gl.framebufferTexture2D(WEBGL.FRAMEBUFFER, WEBGL.COLOR_ATTACHMENT0,
+        WEBGL.TEXTURE_2D, colorTexture.GetTexture(), 0);
+
+    //depthTexture = new TypedTexture("frame::depth", width, height,
+    //    WEBGL.DEPTH_COMPONENT, WEBGL.UNSIGNED_SHORT);
+    //depthTexture.Install(gl);
+    //gl.framebufferTexture2D(WEBGL.FRAMEBUFFER, WEBGL.DEPTH_ATTACHMENT,
+    //    WEBGL.TEXTURE_2D, null, 0);
+
+    //gl.bindTexture(WEBGL.TEXTURE_2D, null);
+    int err = gl.checkFramebufferStatus(WEBGL.FRAMEBUFFER);
+    assert(err == WEBGL.FRAMEBUFFER_COMPLETE);
+    gl.bindFramebuffer(WEBGL.FRAMEBUFFER, null);
+  }
+
   bool ready() {
-    bool result =
-        gl.checkFramebufferStatus(WEBGL.FRAMEBUFFER) == WEBGL.FRAMEBUFFER_COMPLETE;
+    bool result = gl.checkFramebufferStatus(WEBGL.FRAMEBUFFER) ==
+        WEBGL.FRAMEBUFFER_COMPLETE;
     if (!result) {
       print("FRAMEBUFFER_INCOMPLETE");
     }
