cf13cd0203d4447d7bc5c37410829b10700a0d8c
Robert Muth <robert@muth.org>
2016-08-18 13:43:14 -0400

    custom math code retirement

--- lib/chronosmath.dart
+++
@@ -1,9 +0,0 @@
-library chronosmath;
-
-import 'dart:math' as Math;
-import 'dart:typed_data';
-import 'package:vector_math/vector_math.dart' as VM;
-
-// part "src/vector.dart";
-// part "src/matrix4.dart";
-// part "src/quaternion.dart";
