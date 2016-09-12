3f24d35e1be3ad051b99f08ec78e98c23d3ce586
Robert Muth <robert@muth.org>
2016-09-11 23:08:00 -0400

    warning cleanup

--- example/instancer/instancer.dart
+++ example/instancer/instancer.dart
@@ -92,7 +92,8 @@ void main() {
   HTML.window.onResize.listen(resolutionChange);
 
   double _lastTimeMs = 0.0;
-  void animate(double timeMs) {
+  void animate(timeMs) {
+    timeMs = 0.0 + timeMs;
     double elapsed = timeMs - _lastTimeMs;
     _lastTimeMs = timeMs;
     orbit.azimuth += 0.001;
