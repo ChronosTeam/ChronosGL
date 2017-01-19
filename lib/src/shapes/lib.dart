ad1eb4117da7595f44eb2186827d9a97b5b99f97
Robert Muth <robert@muth.org>
2017-01-18 22:54:23 -0500

    cleanup shapes lib

--- lib/src/shapes/lib.dart
+++ lib/src/shapes/lib.dart
@@ -3,12 +3,17 @@ library shapes;
 import 'dart:math' as Math;
 import 'package:vector_math/vector_math.dart' as VM;
 
-// We only depend on this library for GeometryBuilder
+/// The shapes layer contains helpers for create GeometryBuilder
+/// objects for basic shapes like cubes and cylinders.
+/// Higher layer contain wrappers that generate the corresponding
+/// MeshData objects from them.
+///
+/// Dependencies:
+/// base layer (for GeometryBuilder)
 import '../base/lib.dart';
 
-part "shapes.dart";
+part "plane.dart";
 part "cylinder.dart";
 part "cube.dart";
 part "torusknot.dart";
 part "icosahedron.dart";
-
