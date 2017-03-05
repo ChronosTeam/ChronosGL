4b6d42f813e6068ae2524737b9652c521aa1b715
Robert Muth <robert@muth.org>
2017-03-05 14:49:00 -0500

    add new solid color texture helpers

--- lib/src/webhelper/lib.dart
+++ lib/src/webhelper/lib.dart
@@ -4,6 +4,7 @@ import 'dart:html' as HTML;
 import "dart:async";
 import 'dart:convert';
 import "dart:web_gl" as WEBGL;
+import 'package:vector_math/vector_math.dart' as VM;
 
 import '../core/lib.dart';
 
