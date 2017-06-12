3df5fcb935953fb39883f59d617951625bfd9c09
Robert Muth <robert@muth.org>
2017-06-11 22:14:44 -0400

    more work on transform_feedback example

--- example/transform_feedback/fireworks.dart
+++ example/transform_feedback/fireworks.dart
@@ -51,8 +51,7 @@ Math.Random rand = new Math.Random();
 const int kNumFireworkParticles = 200;
 const double kMaxDistance = 100.0;
 const double kMinDistance = 0.2;
-const int kPoles = 10;
-const int kIons = 100000;
+const int kIons = 10000;
 
 String DumpVec(VM.Vector3 v) {
   return "${v.x} ${v.y} ${v.z}";
@@ -87,11 +86,11 @@ class Pole {
 }
 
 class Ion {
-  VM.Vector3 _pos = new VM.Vector3.zero();
+  VM.Vector3 _pos;
   double _speed;
 
   Ion(this._speed, VM.Vector3 pos) {
-    _pos.xyz = pos.xyz;
+    _pos = pos.clone();
   }
 
   VM.Vector3 Pos() => _pos;
@@ -110,7 +109,7 @@ class Ion {
       // stop if we are out of bounds
       if (len > kMaxDistance) {
         Pole p = srcs[rng.nextInt(srcs.length)];
-        _pos.xyz = p.Pos().xyz;
+        _pos = p.Pos() + RandomVector(rng, 20.0 *  dt);
         print("too far ${len}");
         return;
       }
@@ -125,7 +124,7 @@ class Ion {
       // stop if we would hit the pole in the next timestep
       if (len <= kMinDistance) {
         Pole p = srcs[rng.nextInt(srcs.length)];
-        _pos.xyz = p.Pos().xyz;
+        _pos = p.Pos() + RandomVector(rng, 20.0 * dt);
         print("too close: ${len}");
         return;
       }
@@ -168,45 +167,31 @@ void main() {
 
   HTML.CanvasElement canvas = HTML.document.querySelector('#webgl-canvas');
   ChronosGL chronosGL = new ChronosGL(canvas);
-  OrbitCamera orbit = new OrbitCamera(15.0, 0.0, 0.0, canvas);
+  OrbitCamera orbit = new OrbitCamera(15.0, 0.5, 0.5, canvas);
   Perspective perspective = new Perspective(orbit, 0.1, 1000.0);
   RenderPhase phase = new RenderPhase("main", chronosGL);
 
   Math.Random rng = new Math.Random(0);
   List<Pole> srcPoles = [];
-  /*
-  for (int i = 0; i < kPoles; i++) {
-    VM.Vector3 pos = RandomVector(rng, kMaxDistance * 0.1);
-    Pole pole = new Pole(pos)..Update(1.0);
-    srcPoles.add(pole);
-    print("src: ${pole}");
-  }
-  */
-  double p = 2.0;
-  srcPoles.add(new Pole(new VM.Vector3(p, p, p))..Update(1.0));
-  srcPoles.add(new Pole(new VM.Vector3(p, -p, p))..Update(1.0));
-  srcPoles.add(new Pole(new VM.Vector3(-p, p, p))..Update(1.0));
-  srcPoles.add(new Pole(new VM.Vector3(-p, -p, p))..Update(1.0));
+  double p = 3.0;
+  srcPoles.add(new Pole(new VM.Vector3(2.0 * p, 0.0, 2.0 * p))..Update(1.0));
+  srcPoles.add(new Pole(new VM.Vector3(1.0 * p, 0.0, 2.0 * p))..Update(1.0));
+  srcPoles.add(new Pole(new VM.Vector3(0.0 * p, 0.0, 2.0 * p))..Update(1.0));
+  srcPoles.add(new Pole(new VM.Vector3(-1.0 * p, 0.0, 2.0 * p))..Update(1.0));
+  srcPoles.add(new Pole(new VM.Vector3(-2.0 * p, 0.0, 2.0 * p))..Update(1.0));
 
   List<Pole> dstPoles = [];
-  /*
-  for (int i = 0; i < kPoles; i++) {
-    VM.Vector3 pos = RandomVector(rng, kMaxDistance * 0.1);
-    Pole pole = new Pole(pos)..Update(1.0);
-    dstPoles.add(pole);
-    print("dst: ${pole}");
-  }
-  */
-  dstPoles.add(new Pole(new VM.Vector3(p, p, -p))..Update(1.0));
-  dstPoles.add(new Pole(new VM.Vector3(p, -p, -p))..Update(1.0));
-  dstPoles.add(new Pole(new VM.Vector3(-p, p, -p))..Update(1.0));
-  dstPoles.add(new Pole(new VM.Vector3(-p, -p, -p))..Update(1.0));
+  dstPoles.add(new Pole(new VM.Vector3(2.0 * p, 0.0, -2.0 * p))..Update(1.0));
+  dstPoles.add(new Pole(new VM.Vector3(1.0 * p, 0.0, -2.0 * p))..Update(1.0));
+  dstPoles.add(new Pole(new VM.Vector3(0.0 * p, 0.0, -2.0 * p))..Update(1.0));
+  dstPoles.add(new Pole(new VM.Vector3(-1.0 * p, 0.0, -2.0 * p))..Update(1.0));
+  dstPoles.add(new Pole(new VM.Vector3(-2.0 * p, 0.0, -2.0 * p))..Update(1.0));
 
+  VM.Vector3 outOfBounds = new VM.Vector3.all(kMaxDistance * 100.0);
   List<Ion> ions = [];
   for (int i = 0; i < kIons; i++) {
-    Ion ion = new Ion(1.0 * rng.nextDouble(), dstPoles[0].Pos());
+    Ion ion = new Ion(1.0 + 2.0 * rng.nextDouble(), outOfBounds);
     ions.add(ion);
-    print("ion: ${ion}");
   }
   Float32List vertices = new Float32List(3 * kIons);
   Material mat = new Material.Transparent("stars", BlendEquationMix)
@@ -260,6 +245,7 @@ void main() {
     orbit.azimuth += 0.001;
     int n = 0;
     for (Ion ion in ions) {
+      if (elapsed == 0.0) continue;
       ion.Update(srcPoles, dstPoles, rng, elapsed / 1000.0);
       vertices[n + 0] = ion.Pos().x;
       vertices[n + 1] = ion.Pos().y;
